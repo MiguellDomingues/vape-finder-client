38dd086f81821609e871ab4e133cff5e99c8fa61
Miguel Domingues <42451135+MiguellDomingues@users.noreply.github.com>
2023-03-28 12:05:39 -0700

    fixed bug with year field not validating correctly, changed title

--- src/components/vertifyAge/useVertifyAge.js
+++ src/components/vertifyAge/useVertifyAge.js
@@ -16,23 +16,22 @@ function useVertifyAge(){
     const validateInput = e => {
       e.preventDefault()
       
-      const input_str = [ 
+      const input_str = [                                                      // pad d/m with 0-1 zeros, year with 0-4 zeros, then concantenate the strings
         birth_date.year.padStart(4, '0'), 
         birth_date.month.padStart(2, '0'), 
         birth_date.day.padStart(2, '0') 
       ].join('')
-  
-      const age = moment().diff(moment(input_str, 'YYYYMMDD'), 'years') 
-  
-      //still missing some edge cases, such as empty years will close the overlay
-      if(!input_str.match(/^[0-9]+$/)){
+      
+      const age = moment().diff(moment(input_str, 'YYYYMMDD'), 'years')       // validate YYYYMMDD format and get the diff in years from current time
+      
+      if(!input_str.match(/^[0-9]+$/)){                                       // if the date has non-numbers....
         setError("date must contain only numbers")
-      }else if(isNaN(age)){
+      }else if(isNaN(age) || age > 100){                                      // if the days/months are invalid or the year is before 1923
         setError("please enter valid date")
-      }else if(age < MIN_AGE){
+      }else if(age < MIN_AGE){                                                
         setError("you are not old enough to view this content")
       }else{
-        save_vertification.current && localStorage.setItem(STORAGE_KEY, true);
+        save_vertification.current && localStorage.setItem(STORAGE_KEY, true); // save the vertification in session storage if the checkbox is selected
         setShow(false)
       }
     }
