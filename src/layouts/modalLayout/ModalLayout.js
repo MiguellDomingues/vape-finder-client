053b0a623eefbd005eb69d27548ce15279730358
Miguel Domingues <42451135+MiguellDomingues@users.noreply.github.com>
2023-03-30 14:40:24 -0700

    added collpasible menu cmp to mobile model; cleaned css across dt/mobile views

--- src/layouts/modalLayout/ModalLayout.js
+++ src/layouts/modalLayout/ModalLayout.js
@@ -1,8 +1,9 @@
 import { useFilters } from '../../hooks/useFilters.js'
 import  usePillList  from '../../hooks/usePillList'
-import { DropDownMenu, SortByDropDown, PillList, HorizontalLine,CollapsibleMenu } from '../../widgets/widgets.js'
+import { DropDownMenu, SortByDropDown, PillList, HorizontalLine, CollapsibleMenu } from '../../widgets/widgets.js'
 import { FILTER_KEYS } from '../../utils.js'
 import { RiCloseFill } from 'react-icons/ri';
+import { useRef, useEffect, } from 'react'
 
 import './modallayout.css'
 
@@ -12,18 +13,43 @@ function ModalLayout({selected_filters_handlers, filter_tags_query, toggleModal}
     const [ filter_tags,selected_filters,loading,error,{ onFilterTagSelected }] = useFilters(selected_filters_handlers, filter_tags_query)
     const [areFiltersSelected,{handleRemove, handleClear, clearAll}] = usePillList(selected_filters_handlers)
 
+    const modal_overlay = useRef(null)
+
+    useEffect(() => {
+
+      modal_overlay.current.classList.add('fade-in')
+    },[] );
+
+  
+  console.log("MODAL: ", modal_overlay)
+
+    function animateClose(e){
+      e.stopPropagation()
+      if(modal_overlay){                                                     
+        modal_overlay.current.classList.add('fade-out')                         
+        modal_overlay.current.ontransitionend = _ => toggleModal()
+      }else{                                                                     
+        toggleModal()
+      }
+    }
+
+
     if (loading) return 'Loading...';
     if (error) return `Error! ${error.message}`;
 
    const { category_tags, brands_tags, stores_tags } = filter_tags
    const { category, stores, brands, } = selected_filters
 
-    return(<div class="modal" id="modal-wrapper" onClick={e=>e.target?.id === "modal-wrapper" && toggleModal()}>
-    <div class="modal-content">
-      <div className="close" onClick={e=> toggleModal()}><span><RiCloseFill/></span></div>  
+    return(<>
+    <div ref={modal_overlay} className="modal modal_no_select" id="modal-wrapper" onClick={e=>e.target?.id === "modal-wrapper" && animateClose(e)}>
+    <div className="modal-content">
       
-      <div className="filter_pill">
-        <DropDownMenu title="Categories" tags={category_tags} selected_tags={category} selectedHandler={onFilterTagSelected(FILTER_KEYS.CATEGORIES)} />     
+      <div className="close" onClick={e=> animateClose(e)}><span><RiCloseFill/></span></div>  
+      
+      <div className="modal_layout_row">
+      <CollapsibleMenu title="Categories" tags={category_tags} selected_tags={category} selectedHandler={onFilterTagSelected(FILTER_KEYS.CATEGORIES)} />
+       
+        {/*<DropDownMenu title="Categories" tags={category_tags} selected_tags={category} selectedHandler={onFilterTagSelected(FILTER_KEYS.CATEGORIES)} />  */}   
       </div>
 
       <div className="modal_pill_list">
@@ -32,8 +58,9 @@ function ModalLayout({selected_filters_handlers, filter_tags_query, toggleModal}
 
       <HorizontalLine/>
 
-      <div className="filter_pill">
-        <DropDownMenu title="Brands" tags={brands_tags} selected_tags={brands} selectedHandler={onFilterTagSelected(FILTER_KEYS.BRANDS)} />
+      <div className="modal_layout_row">
+      <CollapsibleMenu title="Brands" tags={brands_tags} selected_tags={brands} selectedHandler={onFilterTagSelected(FILTER_KEYS.BRANDS)} />
+        {/*<DropDownMenu title="Brands" tags={brands_tags} selected_tags={brands} selectedHandler={onFilterTagSelected(FILTER_KEYS.BRANDS)} />   */  }
       </div>
       
       <div className="modal_pill_list">
@@ -42,8 +69,9 @@ function ModalLayout({selected_filters_handlers, filter_tags_query, toggleModal}
 
       <HorizontalLine/>
       
-      <div className="filter_pill">
-        <DropDownMenu title="Stores" tags={stores_tags} selected_tags={stores} selectedHandler={onFilterTagSelected(FILTER_KEYS.STORES)} />
+      <div className="modal_layout_row">
+      <CollapsibleMenu title="Stores" tags={stores_tags} selected_tags={stores} selectedHandler={onFilterTagSelected(FILTER_KEYS.STORES)} />
+        {/*<DropDownMenu title="Stores" tags={stores_tags} selected_tags={stores} selectedHandler={onFilterTagSelected(FILTER_KEYS.STORES)} />*/}
       </div>
 
       <div className="modal_pill_list">
@@ -52,12 +80,28 @@ function ModalLayout({selected_filters_handlers, filter_tags_query, toggleModal}
 
       <HorizontalLine/>
 
-      <SortByDropDown selected_filters_handlers={selected_filters_handlers}/>
+      <div className="modal_layout_row">
+        <SortByDropDown selected_filters_handlers={selected_filters_handlers}/>
+      </div>
       {areFiltersSelected && <button onClick={ (e)=>clearAll() }>Clear All Filters</button>}  
-      
+  
     </div>
-
-  </div>)
+</div>
+  </>)
   }
 
   export default ModalLayout
+
+  /*
+  <CollapsibleMenu title="Categories" tags={category_tags} selected_tags={category} selectedHandler={onFilterTagSelected(FILTER_KEYS.CATEGORIES)}  handleClear={handleClear(FILTER_KEYS.CATEGORIES)}/>
+       
+        <HorizontalLine/>
+       
+        <CollapsibleMenu title="Brands" tags={brands_tags} selected_tags={brands} selectedHandler={onFilterTagSelected(FILTER_KEYS.BRANDS)}  handleClear={handleClear(FILTER_KEYS.BRANDS)}/>
+       
+        <HorizontalLine/>
+        
+        <CollapsibleMenu title="Stores" tags={stores_tags} selected_tags={stores} selectedHandler={onFilterTagSelected(FILTER_KEYS.STORES)}  handleClear={handleClear(FILTER_KEYS.STORES)}/>
+       
+        <HorizontalLine/>
+  */
--- src/widgets/widgets.js
+++ src/widgets/widgets.js
@@ -16,8 +16,8 @@ export function SortByDropDown({selected_filters_handlers })  {
           setAndRefetch({...selected_filters})
         }
       
-    return(<div>
-        Sort By:<br/>
+    return(<div className="sortby_dropdown_container">
+        Sort By
         <select className='dropdown' onChange={sortSelected} value={selected_filters.sort_by} >
             <option value={SORT_TYPE.NONE}>None</option>
             <option value={SORT_TYPE.DESC}>Price: High to Low</option>
@@ -35,16 +35,24 @@ export function DropDownMenu( {title, tags, selected_tags, selectedHandler} ){
     //move the selected tags to the top of the dropdown list
     //const sortedtags = [ ...tags.filter( t=> selected_tags.includes(t.tag_name) )]
                       //  .concat([...tags.filter( t=> !selected_tags.includes(t.tag_name)) ])
-
-    return(<div className="dropdown_container cursor_hand">
-    <div className="dropdown_title"> {title}<div className="dropdown-arrow"></div> </div>
+     function toggleMenu(e){ 
+        //e.stopPropagation()
+        //console.log("mouse enter! ", )
+       // let content = e.target.nextElementSibling;
+       // content.style.maxHeight = content.style.maxHeight ? null : "200px"
+    }
     
-    <div className="dropdown-content">    
-        {tags.map( (tag, idx)=>
-            <div className={selectedTagsBGC(tag.tag_name, selected_tags)}
-                  key={idx} 
-                  onClick={ ()=> selectedHandler(tag.tag_name)}>
-                 <div>{tag.tag_name}</div> <div>{tag.product_count}</div>         
+    return(
+    <div className="dropdown_container cursor_hand">
+        <div className="dropdown_title" onMouseEnter={e=>toggleMenu(e)} onMouseLeave={e=>toggleMenu(e)}
+        > {title}<div className="dropdown-arrow"></div> </div>
+            
+        <div className="dropdown-content">    
+            {tags.map( (tag, idx)=>
+                <div className={selectedTagsBGC(tag.tag_name, selected_tags)}
+                    key={idx} 
+                    onClick={ ()=> selectedHandler(tag.tag_name)}>
+                <div>{tag.tag_name}</div> <div>{tag.product_count}</div>         
             </div>)}                  
         </div>
     </div>)
@@ -85,15 +93,15 @@ export function CollapsibleMenu( {title, tags, selected_tags, selectedHandler, r
     function toggleMenu(target){
         target.classList.toggle("active");
         let content = target.nextElementSibling;
-        content.style.maxHeight = content.style.maxHeight ? null : "20%"
+        content.style.maxHeight = content.style.maxHeight ? null : "100px"
         registerMenu && registerMenu(target)
     }
 
     return(<>
-    <button class="collapsible" onClick={e=>toggleMenu(e.target)}>{title}
+    <button className="collapsible" onClick={e=>toggleMenu(e.target)}>{title}
         {handleClear && selected_tags.length > 0 && <button onClick={clearFilters}>Clear</button>}
     </button>
-    <div class="content">
+    <div className="content">
     {tags.map( (tag, idx)=>
             <div className={selectedTagsBGC(tag.tag_name, selected_tags)}
                   key={idx} 
